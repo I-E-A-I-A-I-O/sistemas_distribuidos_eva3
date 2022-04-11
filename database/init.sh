da247893ceb30e0a9494e0a252b5a0c7249b372a
I-E-A-I-A-I-O <jesusdavid268@outlook.com>
2022-04-10 21:47:42 -0400

    database container

---
+++ database/init.sh
@@ -0,0 +1,40 @@
+set -e
+
+psql --username "$POSTGRES_USER" <<-EOSQL
+CREATE DATABASE dist_sys_ev_3;
+EOSQL
+psql --dbname "dist_sys_ev_3" --username "$POSTGRES_USER" <<-EOSQL
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE SCHEMA users;
+CREATE SCHEMA posts;
+CREATE SCHEMA friends;
+CREATE TABLE users.users (
+    user_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    user_name VARCHAR(25) UNIQUE,
+    user_email VARCHAR(75) UNIQUE,
+    user_password VARCHAR(256) NOT NULL,
+    user_role VARCHAR(50) NOT NULL
+);
+CREATE TABLE posts.posts (
+    post_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    post_body VARCHAR(200) NOT NULL,
+    post_owner_id UUID REFERENCES users.users,
+    parent_post_id UUID REFERENCES posts.posts
+);
+CREATE TABLE posts.likes (
+    like_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    like_owner_id UUID REFERENCES users.users,
+    liked_post_id UUID REFERENCES posts.posts
+);
+CREATE TABLE friends.friendships (
+    friendship_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    user_id UUID REFERENCES users.users,
+    friend_id UUID REFERENCES users.users
+);
+CREATE TABLE friends.requests (
+    request_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    request_owner_id UUID REFERENCES users.users,
+    requested_to_id UUID REFERENCES users.users,
+    request_status VARCHAR(25) NOT NULL
+);
+EOSQL
